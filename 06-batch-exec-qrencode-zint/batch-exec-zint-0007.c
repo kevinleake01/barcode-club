b82d6318693f02db054b14154081e874e93bbde0
kevinleake01 <kevinleake01@gmail.com>
2023-02-04 12:45:27 +0000

    Changes of batch-exec-zint-0005.c and batch-exec-zint-0007.c to --eci=0

--- 06-batch-exec-qrencode-zint/batch-exec-zint-0007.c
+++ 06-batch-exec-qrencode-zint/batch-exec-zint-0007.c
@@ -18,30 +18,30 @@
 
 int main(int argc, char *argv[])
 {
-  system("zint -b 71 --eci=26 -o dm_0001.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0002.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0003.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0004.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0005.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0006.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0007.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0008.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0009.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0010.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0011.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0012.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0013.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0014.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0015.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0016.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0017.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0018.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0019.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0020.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0021.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0022.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0023.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0024.png -d \"\"");
-  system("zint -b 71 --eci=26 -o dm_0025.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0001.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0002.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0003.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0004.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0005.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0006.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0007.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0008.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0009.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0010.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0011.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0012.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0013.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0014.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0015.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0016.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0017.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0018.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0019.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0020.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0021.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0022.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0023.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0024.png -d \"\"");
+  system("zint -b 71 --eci=0 -o dm_0025.png -d \"\"");
 }
 
