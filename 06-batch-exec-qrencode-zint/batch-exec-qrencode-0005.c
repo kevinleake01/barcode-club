0885739aa5360f54e1735ec0ca82616d14099185
Kevin Leake <kevinleake01@gmail.com>
2025-03-19 11:19:56 +0000

    06-batch-exec-qrencode-zint updates

---
+++ 06-batch-exec-qrencode-zint/batch-exec-qrencode-0005.c
@@ -0,0 +1,123 @@
+/*
+####################################
+#
+# --- BATCH-EXEC-QRENCODE-0005.C ---
+#
+# This is a batch-exec file to convert text from a file into QR Codes
+# using Qrencode. The files are indexed from qr_0001.txt to qr_0100.txt.
+# Please edit the line of text to suit your needs. The QR Codes will be
+# saved as PNG files.
+#
+# To run the program:
+#
+# gcc batch-exec-qrencode-0005.c; ./a.out
+#
+####################################
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  system("qrencode -8 -o qr_0001.png -r qr_0001.txt");
+  system("qrencode -8 -o qr_0002.png -r qr_0002.txt");
+  system("qrencode -8 -o qr_0003.png -r qr_0003.txt");
+  system("qrencode -8 -o qr_0004.png -r qr_0004.txt");
+  system("qrencode -8 -o qr_0005.png -r qr_0005.txt");
+  system("qrencode -8 -o qr_0006.png -r qr_0006.txt");
+  system("qrencode -8 -o qr_0007.png -r qr_0007.txt");
+  system("qrencode -8 -o qr_0008.png -r qr_0008.txt");
+  system("qrencode -8 -o qr_0009.png -r qr_0009.txt");
+  system("qrencode -8 -o qr_0010.png -r qr_0010.txt");
+  system("qrencode -8 -o qr_0011.png -r qr_0011.txt");
+  system("qrencode -8 -o qr_0012.png -r qr_0012.txt");
+  system("qrencode -8 -o qr_0013.png -r qr_0013.txt");
+  system("qrencode -8 -o qr_0014.png -r qr_0014.txt");
+  system("qrencode -8 -o qr_0015.png -r qr_0015.txt");
+  system("qrencode -8 -o qr_0016.png -r qr_0016.txt");
+  system("qrencode -8 -o qr_0017.png -r qr_0017.txt");
+  system("qrencode -8 -o qr_0018.png -r qr_0018.txt");
+  system("qrencode -8 -o qr_0019.png -r qr_0019.txt");
+  system("qrencode -8 -o qr_0020.png -r qr_0020.txt");
+  system("qrencode -8 -o qr_0021.png -r qr_0021.txt");
+  system("qrencode -8 -o qr_0022.png -r qr_0022.txt");
+  system("qrencode -8 -o qr_0023.png -r qr_0023.txt");
+  system("qrencode -8 -o qr_0024.png -r qr_0024.txt");
+  system("qrencode -8 -o qr_0025.png -r qr_0025.txt");
+  system("qrencode -8 -o qr_0026.png -r qr_0026.txt");
+  system("qrencode -8 -o qr_0027.png -r qr_0027.txt");
+  system("qrencode -8 -o qr_0028.png -r qr_0028.txt");
+  system("qrencode -8 -o qr_0029.png -r qr_0029.txt");
+  system("qrencode -8 -o qr_0030.png -r qr_0030.txt");
+  system("qrencode -8 -o qr_0031.png -r qr_0031.txt");
+  system("qrencode -8 -o qr_0032.png -r qr_0032.txt");
+  system("qrencode -8 -o qr_0033.png -r qr_0033.txt");
+  system("qrencode -8 -o qr_0034.png -r qr_0034.txt");
+  system("qrencode -8 -o qr_0035.png -r qr_0035.txt");
+  system("qrencode -8 -o qr_0036.png -r qr_0036.txt");
+  system("qrencode -8 -o qr_0037.png -r qr_0037.txt");
+  system("qrencode -8 -o qr_0038.png -r qr_0038.txt");
+  system("qrencode -8 -o qr_0039.png -r qr_0039.txt");
+  system("qrencode -8 -o qr_0040.png -r qr_0040.txt");
+  system("qrencode -8 -o qr_0041.png -r qr_0041.txt");
+  system("qrencode -8 -o qr_0042.png -r qr_0042.txt");
+  system("qrencode -8 -o qr_0043.png -r qr_0043.txt");
+  system("qrencode -8 -o qr_0044.png -r qr_0044.txt");
+  system("qrencode -8 -o qr_0045.png -r qr_0045.txt");
+  system("qrencode -8 -o qr_0046.png -r qr_0046.txt");
+  system("qrencode -8 -o qr_0047.png -r qr_0047.txt");
+  system("qrencode -8 -o qr_0048.png -r qr_0048.txt");
+  system("qrencode -8 -o qr_0049.png -r qr_0049.txt");
+  system("qrencode -8 -o qr_0050.png -r qr_0050.txt");
+  system("qrencode -8 -o qr_0051.png -r qr_0051.txt");
+  system("qrencode -8 -o qr_0052.png -r qr_0052.txt");
+  system("qrencode -8 -o qr_0053.png -r qr_0053.txt");
+  system("qrencode -8 -o qr_0054.png -r qr_0054.txt");
+  system("qrencode -8 -o qr_0055.png -r qr_0055.txt");
+  system("qrencode -8 -o qr_0056.png -r qr_0056.txt");
+  system("qrencode -8 -o qr_0057.png -r qr_0057.txt");
+  system("qrencode -8 -o qr_0058.png -r qr_0058.txt");
+  system("qrencode -8 -o qr_0059.png -r qr_0059.txt");
+  system("qrencode -8 -o qr_0060.png -r qr_0060.txt");
+  system("qrencode -8 -o qr_0061.png -r qr_0061.txt");
+  system("qrencode -8 -o qr_0062.png -r qr_0062.txt");
+  system("qrencode -8 -o qr_0063.png -r qr_0063.txt");
+  system("qrencode -8 -o qr_0064.png -r qr_0064.txt");
+  system("qrencode -8 -o qr_0065.png -r qr_0065.txt");
+  system("qrencode -8 -o qr_0066.png -r qr_0066.txt");
+  system("qrencode -8 -o qr_0067.png -r qr_0067.txt");
+  system("qrencode -8 -o qr_0068.png -r qr_0068.txt");
+  system("qrencode -8 -o qr_0069.png -r qr_0069.txt");
+  system("qrencode -8 -o qr_0070.png -r qr_0070.txt");
+  system("qrencode -8 -o qr_0071.png -r qr_0071.txt");
+  system("qrencode -8 -o qr_0072.png -r qr_0072.txt");
+  system("qrencode -8 -o qr_0073.png -r qr_0073.txt");
+  system("qrencode -8 -o qr_0074.png -r qr_0074.txt");
+  system("qrencode -8 -o qr_0075.png -r qr_0075.txt");
+  system("qrencode -8 -o qr_0076.png -r qr_0076.txt");
+  system("qrencode -8 -o qr_0077.png -r qr_0077.txt");
+  system("qrencode -8 -o qr_0078.png -r qr_0078.txt");
+  system("qrencode -8 -o qr_0079.png -r qr_0079.txt");
+  system("qrencode -8 -o qr_0080.png -r qr_0080.txt");
+  system("qrencode -8 -o qr_0081.png -r qr_0081.txt");
+  system("qrencode -8 -o qr_0082.png -r qr_0082.txt");
+  system("qrencode -8 -o qr_0083.png -r qr_0083.txt");
+  system("qrencode -8 -o qr_0084.png -r qr_0084.txt");
+  system("qrencode -8 -o qr_0085.png -r qr_0085.txt");
+  system("qrencode -8 -o qr_0086.png -r qr_0086.txt");
+  system("qrencode -8 -o qr_0087.png -r qr_0087.txt");
+  system("qrencode -8 -o qr_0088.png -r qr_0088.txt");
+  system("qrencode -8 -o qr_0089.png -r qr_0089.txt");
+  system("qrencode -8 -o qr_0090.png -r qr_0090.txt");
+  system("qrencode -8 -o qr_0091.png -r qr_0091.txt");
+  system("qrencode -8 -o qr_0092.png -r qr_0092.txt");
+  system("qrencode -8 -o qr_0093.png -r qr_0093.txt");
+  system("qrencode -8 -o qr_0094.png -r qr_0094.txt");
+  system("qrencode -8 -o qr_0095.png -r qr_0095.txt");
+  system("qrencode -8 -o qr_0096.png -r qr_0096.txt");
+  system("qrencode -8 -o qr_0097.png -r qr_0097.txt");
+  system("qrencode -8 -o qr_0098.png -r qr_0098.txt");
+  system("qrencode -8 -o qr_0099.png -r qr_0099.txt");
+  system("qrencode -8 -o qr_0100.png -r qr_0100.txt");
+}
+
---
+++ 06-batch-exec-qrencode-zint/batch-exec-zint-0019.c
@@ -0,0 +1,123 @@
+/*
+####################################
+#
+# --- BATCH-EXEC-ZINT-0019.C ---
+#
+# This is a batch-exec file to convert text from a file into QR Codes
+# using Zint. The files are indexed from qr_0001.txt to qr_0100.txt.
+# Please edit the line of text to suit your needs. The QR Codes will
+# be saved as PNG files.
+#
+# To run the program:
+#
+# gcc batch-exec-zint-0019.c; ./a.out
+#
+####################################
+*/
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0001.png -i qr_0001.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0002.png -i qr_0002.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0003.png -i qr_0003.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0004.png -i qr_0004.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0005.png -i qr_0005.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0006.png -i qr_0006.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0007.png -i qr_0007.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0008.png -i qr_0008.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0009.png -i qr_0009.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0010.png -i qr_0010.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0011.png -i qr_0011.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0012.png -i qr_0012.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0013.png -i qr_0013.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0014.png -i qr_0014.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0015.png -i qr_0015.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0016.png -i qr_0016.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0017.png -i qr_0017.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0018.png -i qr_0018.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0019.png -i qr_0019.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0020.png -i qr_0020.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0021.png -i qr_0021.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0022.png -i qr_0022.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0023.png -i qr_0023.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0024.png -i qr_0024.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0025.png -i qr_0025.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0026.png -i qr_0026.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0027.png -i qr_0027.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0028.png -i qr_0028.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0029.png -i qr_0029.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0030.png -i qr_0030.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0031.png -i qr_0031.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0032.png -i qr_0032.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0033.png -i qr_0033.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0034.png -i qr_0034.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0035.png -i qr_0035.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0036.png -i qr_0036.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0037.png -i qr_0037.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0038.png -i qr_0038.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0039.png -i qr_0039.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0040.png -i qr_0040.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0041.png -i qr_0041.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0042.png -i qr_0042.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0043.png -i qr_0043.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0044.png -i qr_0044.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0045.png -i qr_0045.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0046.png -i qr_0046.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0047.png -i qr_0047.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0048.png -i qr_0048.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0049.png -i qr_0049.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0050.png -i qr_0050.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0051.png -i qr_0051.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0052.png -i qr_0052.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0053.png -i qr_0053.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0054.png -i qr_0054.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0055.png -i qr_0055.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0056.png -i qr_0056.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0057.png -i qr_0057.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0058.png -i qr_0058.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0059.png -i qr_0059.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0060.png -i qr_0060.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0061.png -i qr_0061.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0062.png -i qr_0062.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0063.png -i qr_0063.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0064.png -i qr_0064.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0065.png -i qr_0065.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0066.png -i qr_0066.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0067.png -i qr_0067.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0068.png -i qr_0068.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0069.png -i qr_0069.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0070.png -i qr_0070.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0071.png -i qr_0071.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0072.png -i qr_0072.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0073.png -i qr_0073.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0074.png -i qr_0074.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0075.png -i qr_0075.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0076.png -i qr_0076.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0077.png -i qr_0077.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0078.png -i qr_0078.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0079.png -i qr_0079.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0080.png -i qr_0080.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0081.png -i qr_0081.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0082.png -i qr_0082.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0083.png -i qr_0083.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0084.png -i qr_0084.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0085.png -i qr_0085.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0086.png -i qr_0086.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0087.png -i qr_0087.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0088.png -i qr_0088.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0089.png -i qr_0089.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0090.png -i qr_0090.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0091.png -i qr_0091.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0092.png -i qr_0092.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0093.png -i qr_0093.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0094.png -i qr_0094.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0095.png -i qr_0095.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0096.png -i qr_0096.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0097.png -i qr_0097.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0098.png -i qr_0098.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0099.png -i qr_0099.txt");
+  system("zint -b 58 --whitesp=25 --vwhitesp=25 --eci=26 -o qr_0100.png -i qr_0100.txt");
+}
+
